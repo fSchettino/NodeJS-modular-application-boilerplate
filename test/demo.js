85613a53519d0e04c7c24738d8db6b35e3b41cab
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-08-05 11:14:58 +0200

    demo test deleted

--- test/demo.js
+++
@@ -1,9 +0,0 @@
-export function matchString(string1, string2) {
-  let result = '';
-  if (string1 === string2) {
-    result = 'Match!';
-  } else {
-    result = 'No match!';
-  }
-  return result;
-}
